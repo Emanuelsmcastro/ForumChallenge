4bda8d0aa178f866980ac2556792fc094ee42377
Emanuel Castro <emanuelsmcastro3@gmail.com>
2024-05-16 15:36:18 -0300

    Create BeansConfiguration.java

---
+++ forumchallenge/src/main/java/com/oracle/challenge/forum/forumchallenge/infra/config/BeansConfiguration.java
@@ -0,0 +1,8 @@
+package com.oracle.challenge.forum.forumchallenge.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfiguration {
+    
+}
