826de3df532359490cde55fbdd373fab8840ff22
Emanuel Castro <emanuelsmcastro3@gmail.com>
2024-05-16 10:55:44 -0300

    initial commit

---
+++ forumchallenge/src/main/java/com/oracle/challenge/forum/forumchallenge/ForumchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.challenge.forum.forumchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumchallengeApplication.class, args);
+	}
+
+}
---
+++ forumchallenge/src/test/java/com/oracle/challenge/forum/forumchallenge/ForumchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.challenge.forum.forumchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
